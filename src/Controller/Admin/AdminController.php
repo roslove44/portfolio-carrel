1ad1b3ce3529e62f2d1b90dd432107c4afe66a99
roslove44 <rostandmigan68@gmail.com>
2023-10-22 01:39:01 +0200

    IsGranted Admin

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -8,7 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted("ROLE_ADMIN")]
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
